c3ba39862a6ce777a36dbcfe1edc88794e603901
Ammon1 <kgrod@tlen.pl>
2020-01-13 10:49:22 +0100

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -12,6 +12,10 @@ from scipy import interpolate
 from scipy.fftpack import fft
 from scipy.signal import blackman
 
+def ica_plot(X,a0,a1,a2,a3,a4,a5,a6,a7,a8,a9):
+        y=X[:,0]*a0+X[:,1]*a1+X[:,2]*a2+X[:,3]*a3+X[:,4]*a4+X[:,5]*a5
+        y+=X[:,6]*a6+X[:,7]*a7+X[:,8]*a8+X[:,9]*a9
+        return y
     
 def fft_frame(df):
     x=df.iloc[:,1].values
@@ -57,7 +61,7 @@ def make_spectrum(frame):
     y=np.arange(0,(25/1000)*spectrum.shape[1],(25/1000))
     x=np.around(x,decimals=2)
     y=np.around(y,decimals=2)
-    spectrum.columns=y
+    #spectrum.columns=y
     spectrum=spectrum.set_index(x)
     spectrum=spectrum.T
     #fig = plt.figure(figsize=(6,6), dpi=200)
@@ -65,25 +69,41 @@ def make_spectrum(frame):
     return spectrum,x,y
 
 from scipy.signal import savgol_filter   
-def func(x,x0,A,tau):
-    return A*np.exp(-(x-x0)/tau)
+def func(x,x0,x1,A0,A1,tau0,tau1):
+    return A0*np.exp(-(x-x0)/tau0)+A1*np.exp(-(x-x1)/tau1)
 
-path =r'C:\Users\Administrator\Desktop\Kacper\2019\11\5'
-filename='HgCdTe_70K'
+
+path =r'C:\Users\Administrator\Desktop\Kacper\2020\1\13'
+filename='HgCdTe_4734_115K_mala_moc'
 frame=read_files(path,filename)
 frame=frame.groupby(0, as_index=False).mean()
-frame_int=interp(frame,1/interp)
-sns.heatmap(frame.iloc[:,100:500],cmap='hsv') 
-
-
 spectrum,x,y=make_spectrum(frame)
-plt.plot(np.log(spectrum.loc[:,2186]))
 
-x_plot=y[1000:4000]
-y_plot= spectrum.loc[25:99.98,2186].values
+sns.heatmap(np.log(spectrum.iloc[:,180:200]),cmap='hsv')
+sns.heatmap(np.log(spectrum_int.iloc[:,160:200]),cmap='hsv')
 
-popt,pcov=curve_fit(func, x_plot, y_plot,p0=[0,40000,20]
-                    ,bounds=([0,0,0],[25,1e6,200]))
+
+spectrum_win=spectrum.rolling(50).mean()
+sns.heatmap(np.log(spectrum_win.iloc[:,100:300]),cmap='hsv')
+sns.heatmap(np.log(spectrum_win.iloc[:,170:200]),cmap='hsv')
+plt.plot(np.log(spectrum.loc[:,2312]))
+plt.plot(np.log(spectrum.loc[:,2299]))
+
+plt.plot(np.log(spectrum.loc[:,629]+spectrum.loc[:,2719]+spectrum.loc[:,2732]+spectrum.loc[:,2630]))
+plt.plot(np.log(spectrum_win.iloc[:,190]))
+plt.plot(np.log(spectrum_win.iloc[:,191]))
+plt.plot(np.log(spectrum_win.iloc[:,192]))
+plt.plot(np.log(spectrum_win.iloc[:,193]))
+plt.xlim(100,500)
+plt.ylim(-4,0)
+
+y_plot=spectrum_win.loc[:,2288]
+x_plot=y[50:-300]
+y_plot= (y_plot.values)[50:-300]
+x_plot=np.flip(x_plot)
+
+popt,pcov=curve_fit(func, x_plot, y_plot,p0=[0,0,4,4,20,20]
+                    ,bounds=([0,0,0,0,0,0],[25,25,200,200,800,800]))
 plt.plot(x_plot,y_plot)
 plt.plot(x_plot,func(x_plot,*popt))
 
